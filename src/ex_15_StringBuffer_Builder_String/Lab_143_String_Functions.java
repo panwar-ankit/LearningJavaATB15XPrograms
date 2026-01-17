70a98240c228fab9dd49e7624700d2cd47f41e23
panwar-ankit <ankit4438@gmail.com>
2026-01-17 20:27:20 +0530

    Added new file 17 Jan 2026.

--- src/ex_15_StringBuffer_Builder_String/Lab_143_String_Functions.java
+++ src/ex_15_StringBuffer_Builder_String/Lab_143_String_Functions.java
@@ -2,6 +2,15 @@ package ex_15_StringBuffer_Builder_String;
 
 public class Lab_143_String_Functions {
     static void main(String[] args) {
+        String s0 = "Ankit";
+        String s1 = new String("Ankit");
+
+        // less than 10%
+        StringBuffer stringBuffer = new StringBuffer("Ankit");
+        StringBuilder stringBuilder = new StringBuilder("Ankit");
+
+        System.out.println(stringBuilder.reverse());
+        System.out.println(stringBuffer.reverse());
 
     }
 }
